b5971a8357ce946a37c6bf4587394b173a770202
Adlet Bakyt <bakyt_adlet@mail.ru>
2020-12-12 20:39:11 +0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  
+  favImg = '../../assets/icon/favourite.svg'
+  favImgChacked = '../../assets/icon/favourite-red.svg'
+  recomendArr = [ {checked: false, icon: this.favImg }, 
+                  { checked: false, icon: this.favImg}, 
+                  {checked: false, icon: this.favImg}]
+  
+  newArr =       [ {checked: false, icon: this.favImg }, 
+                  { checked: false, icon: this.favImg}, 
+                  {checked: false, icon: this.favImg}]
+   
   constructor() {}
+
+  
+  categories = {
+    slidesPerView: 4.5  
+  }
+  recommend = {
+    slidesPerView: 2.5
+  }
+  promotions = {
+    slidesPerView: 1.8
+  }
+  store = {
+    slidesPerView: 2.5
+  }
+
+  select(arr){
+    arr.checked = !arr.checked
+
+    if (arr.checked === true){
+      arr.icon = this.favImgChacked
+    } else {
+      arr.icon = this.favImg
+    }      
+
+  }
 
 }
